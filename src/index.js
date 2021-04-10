403377bca764c0c0d711653047f84494491d1653
Kieu kieu <kieudinhthai36200@gmail.com>
2021-04-11 00:12:07 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require("path");
 const app = express()
 const port = 3000
-
+const methordOverride = require("method-override");
 const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, "public")));
