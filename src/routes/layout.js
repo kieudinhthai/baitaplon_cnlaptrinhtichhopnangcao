2ae9ecc722bb9977db46dffd037e024599778766
tuansieunhan2000 <trananhtuan.kg2000@gmail.com>
2021-05-04 15:23:52 +0700

    update ProductDetail and fix layout

--- src/routes/layout.js
+++ src/routes/layout.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const layoutController = require('../controllers/layoutController')
-router.get('/', layoutController.show)
 router.get('/detail', layoutController.show_detail)
+router.get('/', layoutController.show)
 
 module.exports = router
