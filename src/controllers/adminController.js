6f2d212805a6533b2aeea370d7a094a65897f383
unknown <kieudinhthai36200@gmail.com>
2021-05-19 17:12:06 +0700

    97%

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,7 +1,6 @@
 const Product = require("./models/products")
 const fs = require('fs');
-const Acount = require("./models/acount")
-
+const Account = require("./models/account")
 const Category = require("./models/categories")
 const { multipleMongooseToObject } = require("../util/mongoose");
 const { mongooseToObject } = require("../util/mongoose")
@@ -11,15 +10,15 @@ const multer = require("multer")
 class adminController {
     // [GET] /admin/
     index(req, res, next) {
-        let product, products, categories
+        let account, products, categories
         console.log('index');
         console.log(req.session.userId);
-        Acount
+        Account
         .findOne({_id: req.session.userId})
             .then(
-                productObj=>{
-                    product = productObj
-                    console.log(product);
+                accountObj=>{
+                    account = accountObj
+                    console.log(account);
                 }
             )
         .then(()=>{
@@ -30,11 +29,11 @@ class adminController {
             Product.find({deletedAt:null})
                 .then(productList=>{
                     products = Object.values(multipleMongooseToObject(productList))
-                   if (!product) {
-                       res.render('admin/login',{message:"Bạn chưa đăng nhập"})
+                   if (!account) {
+                       res.redirect('/admin/login')
                    }
                    else{
-                    res.render('admin/index',{products, categories})
+                    res.render('admin/index',{products,categories})
                    }
                    
                 })
@@ -45,15 +44,15 @@ class adminController {
 
 //------------------getproducts
     getProducts(req, res, next){
-        let product
-        let productsArray=[]
-        let categoriesArray=[]
-        Acount
+        let account, productsArray,categoriesArray
+        console.log('index');
+        console.log(req.session.userId);
+        Account
         .findOne({_id: req.session.userId})
             .then(
-                productObj=>{
-                    product = productObj
-                    console.log(product);
+                accountObj=>{
+                    account = accountObj
+                    console.log(account);
                 }
             )
         .then(()=>{
@@ -67,8 +66,8 @@ class adminController {
                 categoriesArray = Object.values(multipleMongooseToObject(categories))
             })
             .then(()=>{
-                if (!product) {
-                    res.render('admin/login',{message:"Bạn chưa đăng nhập"})
+                if (!account) {
+                    res.redirect('/admin/login')
                 }
                 else{
                     res.render('admin/adminProducts',{
@@ -92,7 +91,7 @@ class adminController {
     }
     
     login(req, res, next) {
-        Acount
+        Account
         .findOne({user:req.body.username},function(err,data){
             if(data){
                 if(data.password==req.body.password){
@@ -113,22 +112,24 @@ class adminController {
 
 
     getCategories(req, res, next){
-        let categories, product
-        Acount
+        let account,  categories
+        console.log('index');
+        console.log(req.session.userId);
+        Account
         .findOne({_id: req.session.userId})
             .then(
-                productObj=>{
-                    product = productObj
-                    console.log(product);
+                accountObj=>{
+                    account = accountObj
+                    console.log(account);
                 }
             )
         .then(()=>{
         Category
-        .find({})
+        .find({deletedAt:null})
         .then(category=>{
             categories = Object.values(multipleMongooseToObject(category))
-            if (!product) {
-                res.render('admin/login',{message:"Bạn chưa đăng nhập"})
+            if (!account) {
+                res.redirect('/admin/login')
             }
             else{
                 res.render('admin/adminCategories',{categories})
@@ -196,8 +197,18 @@ class adminController {
 }
 // [put] admin/updateProduct
 productDetail(req, res ,next ){
-    let product,categoriesArray
-    // console.log("product detail")
+    let account, product,categoriesArray
+    console.log('index');
+    console.log(req.session.userId);
+    Account
+    .findOne({_id: req.session.userId})
+        .then(
+            accountObj=>{
+                account = accountObj
+                console.log(account);
+            }
+        )
+    .then(()=>{
     Product.findOne({deletedAt:null,_id: req.query.key})
         .then(products=>{
             // console.log(products)
@@ -209,12 +220,20 @@ productDetail(req, res ,next ){
                 categoriesArray = Object.values(multipleMongooseToObject(categories));
             })
             .then(()=>{
-                res.render("admin/adminProductsDetail",{
-                    product,categoriesArray
-                })
+                
+                if (!account) {
+                    res.redirect('/admin/login')
+                }
+                else{
+                    res.render("admin/adminProductsDetail",{
+                        product,categoriesArray
+                    })
+                }
             })
         })
 
+    })
+    .catch(next)
 }
 // 
 productUpdate(req, res, next) {
@@ -270,7 +289,8 @@ insertCategory(req, res, next){
     .then(()=>{
         console.log("newId ->: "+(newId++))
         var add = new Category({
-            name:categoryName,
+            name:req.body.name,
+            vn_name:req.bodyvn_name,
             id:newId++
         }) //tạo 1 object mới
         add.id = newId // gán 
@@ -305,28 +325,33 @@ insertCategory(req, res, next){
     };
 
 //---newbin
+
+        
             getBin(req, res, next) {
-                let product, products
+                let account, products, categories
                 console.log('index');
                 console.log(req.session.userId);
-                Acount
+                Account
                 .findOne({_id: req.session.userId})
                     .then(
-                        productObj=>{
-                            product = productObj
-                            console.log(product);
+                        accountObj=>{
+                            account = accountObj
+                            console.log(account);
                         }
                     )
-                
+                .then(()=>{
+                 Category.find({deleted:true})
+                    .then(category => categories = multipleMongooseToObject(category))
+                })
                 .then(()=>{
                     Product.find({deleted:true})
                         .then(productList=>{
                             products = Object.values(multipleMongooseToObject(productList))
-                           if (!product) {
-                               res.render('admin/login',{message:"Bạn chưa đăng nhập"})
+                           if (!account) {
+                            res.redirect('/admin/login')
                            }
                            else{
-                            res.render('admin/recycle_bin',{products})
+                            res.render('admin/recycle_bin',{products, categories})
                            }
                            
                         })
@@ -334,25 +359,23 @@ insertCategory(req, res, next){
                 })
                 .catch(next)
             };
-        
-       
 
 
-    //[DELETE] /admin/delete
+    //[DELETE] /admin/delete  ------product
     delete(req,res, next){
      Product.delete({_id:req.params.id})
      .then(()=>res.redirect('/admin/'))
      .catch(next)
     }
 
-  //[DELETE] /admin/delete/:id/force
+  //[DELETE] /admin/delete/:id/force---product
   forceDelete(req, res, next) {
     Product.deleteOne({ _id: req.params.id })
-      .then(() => res.redirect('/admin/'))
+      .then(() => res.redirect('/admin/recycle_bin'))
       .catch(next);
   }
   
-   //[PATCH] /admin/restore/:id
+   //[PATCH] /admin/product/restore/:id  -----products
 
    restore(req, res, next) {
     Product.restore({ _id: req.params.id })
@@ -360,7 +383,65 @@ insertCategory(req, res, next){
       .catch(next);
   }
 
- 
+  //----------------------------------category---------------------------
+
+    //[DELETE] /admin/category/delete  ------category
+    delete_cat(req,res, next){
+        Category.delete({_id:req.params.id})
+        .then(()=>res.redirect('/admin/categories'))
+        .catch(next)
+       }
+   
+    //---category
+     forceDelete_cat(req, res, next) {
+        Category.deleteOne({ _id: req.params.id })
+         .then(() => res.redirect('/admin/categories'))
+         .catch(next);
+     }
+     
+      // -----cagegory
+   
+      restore_cat(req, res, next) {
+        Category.restore({ _id: req.params.id })
+         .then(() => res.redirect("/admin/categories"))
+         .catch(next);
+     }
+ //-------------------------search 
+ search(req, res, next) {
+    let account, products, categories
+    console.log('index');
+    console.log(req.session.userId);
+    Account
+    .findOne({_id: req.session.userId})
+        .then(
+            accountObj=>{
+                account = accountObj
+                console.log(account);
+            }
+        )
+    .then(()=>{
+     Category.find({})
+        .then(category => categories = multipleMongooseToObject(category))
+    })
+    .then(()=>{
+        Product.find({deletedAt:null,name: new RegExp(req.query.q, 'i')})
+            .then(productList=>{
+                products = Object.values(multipleMongooseToObject(productList))
+               if (!account) {
+                   res.redirect('/admin/login')
+               }
+               else{
+                res.render('admin/index',{products,categories})
+               }
+               
+            })
+            .catch(next)
+    })
+    .catch(next)
+};
+
+
+
   error(req, res, next){
     res.render('404')
  }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,17 +4,20 @@ const store = require("../util/multer")
 
 const adminController = require('../controllers/adminController')
 
-router.post('/',adminController.login)
+router.post('/login',adminController.login)
 router.get('/', adminController.index)
 router.get('/categories', adminController.getCategories)    
 router.get('/insert', adminController.getProducts)
 router.get('/recycle_bin', adminController.getBin)
 router.get('/login', adminController.showlogin)
 router.get('/logout', adminController.logout)
-router.delete('/delete/:id',adminController.delete)
-router.delete('/delete/:id/force',adminController.forceDelete)
-router.patch('/restore/:id',adminController.restore)
+router.delete('/delete/product/:id',adminController.delete)
+router.delete('/delete/product/:id/force',adminController.forceDelete)
+router.patch('/restore/product/:id',adminController.restore)
 
+router.delete('/delete/category/:id',adminController.delete_cat)
+router.delete('/delete/category/:id/force',adminController.forceDelete_cat)
+router.patch('/restore/category/:id',adminController.restore_cat)
 
 
 // router.get('/insert', adminController.waitInsert)
@@ -24,6 +27,7 @@ router.post('/insertCategories', adminController.insertCategory)
 // router.get('/products/?:productDetail',adminController.productDetail)
 router.get('/productDetail?:key',adminController.productDetail)
 router.put('/productUpdate',store.single('image'),adminController.productUpdate)
+router.get('/search', adminController.search)
 router.get('/:slug', adminController.error)
 
 module.exports = router
